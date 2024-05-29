4386e2f5e2d5111e353339b4bc262d1674b7f899
Rishabh pratap singh <156455104+rishabh434@users.noreply.github.com>
2024-05-30 02:48:45 +0530

    Add files via upload

---
+++ Linkedin-Connection-Bot-Script1_gui/Linkedin-Connection-Bot-Script1_gui/Run_me.py
@@ -0,0 +1,9 @@
+'''
+Author - Rishabh Pratap Singh
+Profile - Data Scientist AI
+'''
+
+
+from Scripts import graphical_script
+if __name__ == '__main__':
+    graphical_script.guibuild()
---
+++ Linkedin-Connection-Bot-Script1_gui/Linkedin-Connection-Bot-Script1_gui/Scripts/constants.py
@@ -0,0 +1,25 @@
+'''
+Author - Rishabh Pratap Singh
+Profile - Data Scientist AI
+'''
+
+username = "anupamkapoor1901@gmail.com"
+commaseparated = "accenture"
+password = "Anupam1234@@"
+
+connection_mess = ''
+# upto pages is just to let the script know for how many pages you want it to execute
+upto_page = 1
+
+
+# Just to add some colors in text
+class Bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
